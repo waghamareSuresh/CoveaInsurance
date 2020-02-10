e51e44c29b03d37888099c36f453d32ba6bb5fdf
suresh <sureshwaghamare1@gmail.com>
2020-02-10 17:41:31 +0530

    Hi team This is Suresh..I commited TC1

---
+++ src/com/test/TC1.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class TC1 {
+
+	public static void main(String[] args) {
+		String str="Suresh123Goutam";
+		
+		System.out.println("Before removing char the string is: "+str);
+
+		
+		String output=str.replaceAll("[^0-9]", "");
+		
+		System.out.println("After removing char the string is : "+output);
+
+	}
+
+	}
+
+
