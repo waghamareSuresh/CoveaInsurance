36e9b477725c4d4b8d9f11946c930915a8844fd4
Shaik Siddhique <siddhiq4043@gmail.com>
2020-02-10 18:13:55 +0530

    Hi Suresh . I changed code as you expected

--- src/com/test/TC2.java
+++ src/com/test/TC2.java
@@ -4,7 +4,7 @@ public class TC2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sureh is a nice person");
+		System.out.println("Selenium is easy");
 
 	}
 
